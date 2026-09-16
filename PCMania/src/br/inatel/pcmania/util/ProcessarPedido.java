b537b056f0a000e4754fc227f73e7880752becbc
pedro alvarenga <73392937+alvarengapz@users.noreply.github.com>
2026-09-16 19:29:01 -0300

    Add files via upload

---
+++ PCMania/src/br/inatel/pcmania/util/ProcessarPedido.java
@@ -0,0 +1,10 @@
+package br.inatel.pcmania.util;
+
+import br.inatel.pcmania.modelo.Computador;
+
+public class ProcessarPedido {
+
+    public static void helper(Computador[] computadores) {
+        System.out.println("Pedido enviado...");
+    }
+}
